3cad8b20db5cf1e69f8e96913398665f48f8bac7
nbond211 <nicholaskbond@gmail.com>
2022-01-19 12:14:02 -0500

    Improve the way we get photos - I hope no one sees my embarrasing list of search terms *blushes*

--- utils/GetProfilePictureOptions.js
+++ utils/GetProfilePictureOptions.js
@@ -2,45 +2,6 @@ import * as nodeFetch from 'node-fetch';
 global.fetch = nodeFetch;
 
 import { createApi } from 'unsplash-js';
-import lodash from 'lodash';
-
-const queryTerms = [
-    'hairy man',
-    'powerful lady',
-    'queer woman',
-    'small man',
-    'business woman',
-    'mustache man',
-    'nonbinary person',
-    'human',
-    'weak man',
-    'muscle woman',
-    'punk rock chick',
-    'goth man',
-    'fatherly man',
-    'princess',
-    'rich woman',
-    'woman doctor',
-    'woman ceo',
-    'woman lawyer',
-    'stripper',
-    'gardner',
-    'chef',
-    'nerd',
-    'nerd woman',
-    'janitor',
-    'man with flowers',
-    'man holding fish',
-    'fraternity bro',
-    'female scientist',
-    'pop star',
-    'personal trainer',
-    'italian man',
-    'gnome',
-    'gamer girl',
-    'portrait',
-    'headshot'
-];
 
 const serverApi = createApi({
     accessKey: '-SntHO69kzosPKpSpJ7VQfLobD2BFK9k55Qm-LC-8Ek',
@@ -48,19 +9,11 @@ const serverApi = createApi({
 });
 
 export const getProfilePictureOptions = async () => {
-    const requests = queryTerms.map(
-        async term =>
-            await serverApi.search.getPhotos({
-                query: term,
-                per_page: 100
-            })
-    );
+    const data = await serverApi.photos.getRandom({
+        count: 30,
+        collections:
+            '3678902,4172658,418611,1060022,8983925,2074689,516966,47557894,75593519,30270697,89335907'
+    });
 
-    const goo = await Promise.all(requests);
-    const foo = lodash.flattenDeep(goo.map(data => data.response.results));
-
-    return lodash.sampleSize(
-        foo.map(image => image?.urls?.small),
-        15
-    );
+    return data.response.map(image => image?.urls?.small);
 };
